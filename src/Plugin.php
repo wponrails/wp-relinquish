1828fc3b1026b6478bdebb853daeb6df244d85b2
Sebastiaan de Geus <sebastiaan@hoppinger.com>
2015-02-17 19:54:51 +0100

    use [] instead of array(), forget about php 5.3, this is the future

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,7 +5,7 @@ namespace Hoppinger\WordPress\Relinquish;
 use GuzzleHttp\Client;
 
 class Plugin {
-  public $synched_types = array();
+  public $synched_types  = [];
   public $relinquish_url = '';
 
   public function __construct() {
@@ -23,25 +23,25 @@ class Plugin {
   }
 
   public function filters() {
-    add_filter( 'json_prepare_post', array( $this, 'wp_api_acf_json_prepare_post' ), 10, 3 );
+    add_filter( 'json_prepare_post', [ $this, 'wp_api_acf_json_prepare_post' ], 10, 3 );
   }
 
   public function actions() {
     // send headers to allow the adminbar to be loaded above rails
-    add_action( 'send_headers', array( $this, 'send_headers' ) );
+    add_action( 'send_headers', [ $this, 'send_headers' ] );
 
-    add_action( 'plugins_loaded', array( $this, 'synch_types' ) );
+    add_action( 'plugins_loaded', [ $this, 'synch_types' ] );
 
     // content editing actions
-    add_action( 'insert_post', array( $this, 'save_post' ), 10, 3 );
-    add_action( 'save_post', array( $this, 'save_post' ), 10, 3 );
-    // add_action( 'wp_trash_post', array( $this, 'before_trash_post' ) );
-    add_action( 'trashed_post', array( $this, 'after_trash_post' ) );
-    add_action( 'delete_post', array( $this, 'before_delete_post' ) );
+    add_action( 'insert_post', [ $this, 'save_post' ], 10, 3 );
+    add_action( 'save_post', [ $this, 'save_post' ], 10, 3 );
+
+    add_action( 'trashed_post', [ $this, 'after_trash_post' ] );
+    add_action( 'delete_post', [ $this, 'before_delete_post' ] );
 
     // hook attachments
-    add_action( 'edit_attachment', array( $this, 'save_attachment' ) );
-    add_action( 'add_attachment', array( $this, 'save_attachment' ) );
+    add_action( 'edit_attachment', [ $this, 'save_attachment' ] );
+    add_action( 'add_attachment', [ $this, 'save_attachment' ] );
   }
 
   public function synch_types() {
@@ -71,9 +71,9 @@ class Plugin {
 
     $client = new Client();
 
-    $response = $client->post( RELINQUISH_URL . "/wp-connector/{$post->post_type}/", array(
-        'body' => array( 'ID' => $post_id ),
-      ) );
+    $response = $client->post( RELINQUISH_URL . "/wp-connector/{$post->post_type}/", [
+      'body' => [ 'ID' => $post_id ],
+      ] );
 
     return true;
   }
@@ -101,9 +101,9 @@ class Plugin {
 
     $url = RELINQUISH_URL . "/wp-connector/{$post->post_type}/";
 
-    $response = $client->post( $url, array(
-        'body' => array( 'ID' => $post_id ),
-      ) );
+    $response = $client->post( $url, [
+      'body' => array( 'ID' => $post_id ),
+      ] );
 
     return true;
   }
@@ -160,7 +160,7 @@ class Plugin {
 
   public function wp_api_acf_json_prepare_post( $_post, $post, $context ) {
 
-    $_post['acf_fields'] = array();
+    $_post['acf_fields'] = [];
 
     if ( $fields = get_fields( $post['ID'] ) ) {
       $_post['acf_fields'] = $fields;
